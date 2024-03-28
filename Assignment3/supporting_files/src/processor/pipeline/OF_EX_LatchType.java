f645cafb6ab52238527e0a898597ade4a0fc3ef9
K12TK013A1N <shourya15singh7@gmail.com>
2024-03-29 02:54:05 +0530

    OF_EX_LatchType.java updated

--- Assignment3/supporting_files/src/processor/pipeline/OF_EX_LatchType.java
+++ Assignment3/supporting_files/src/processor/pipeline/OF_EX_LatchType.java
@@ -3,6 +3,11 @@ package processor.pipeline;
 public class OF_EX_LatchType {
 	
 	boolean EX_enable;
+	int rs1;
+	int rs2;
+	int rd;
+	int immediate;
+	int branchTarget;
 	
 	public OF_EX_LatchType()
 	{
@@ -15,6 +20,46 @@ public class OF_EX_LatchType {
 
 	public void setEX_enable(boolean eX_enable) {
 		EX_enable = eX_enable;
+	}
+
+	public int getRs1(){
+		return rs1;
+	}
+
+	public void setRs1(int Rs1){
+		rs1 = Rs1;
+	}
+
+	public int getRs2(){
+		return rs2;
+	}
+
+	public void setRs2(int Rs2){
+		rs2 = Rs2;
+	}
+
+	public int getRd(){
+		return rd;
+	}
+
+	public void setRd(int Rd){
+		rd = Rd;
+	}
+
+	public int getImm(){
+		return immediate;
+	}
+
+	public void setImm(int imm){
+		immediate = imm;
+	}
+
+	public int getBranchTarget(){
+		return branchTarget;
+	}
+
+	public void setBranchTarget(int brnchT){
+		branchTarget = brnchT;
 	}
 
 }
