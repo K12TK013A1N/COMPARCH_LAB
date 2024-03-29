936f03e4652de6862dab17f4ae8671f845c4099e
K12TK013A1N <shourya15singh7@gmail.com>
2024-03-29 20:00:48 +0530

    MemoryAccess.java updated.

--- Assignment3/supporting_files/src/processor/pipeline/MemoryAccess.java
+++ Assignment3/supporting_files/src/processor/pipeline/MemoryAccess.java
@@ -1,5 +1,6 @@
 package processor.pipeline;
 
+import generic.Instruction.OperationType;
 import processor.Processor;
 
 public class MemoryAccess {
@@ -16,7 +17,37 @@ public class MemoryAccess {
 	
 	public void performMA()
 	{
-		//TODO
+		int rd = EX_MA_Latch.getRd();
+		int rs1 = EX_MA_Latch.getRs1();
+		int rs2 = EX_MA_Latch.getRs2();
+		int rem = EX_MA_Latch.getrem();
+		String Instruction = EX_MA_Latch.getInstruction();
+		int aluresult = EX_MA_Latch.getAluResult();
+		int Ldresult = 0;
+		OperationType operation = OperationType.values()[Integer.parseInt(Instruction.substring(0,5),2)];
+		switch (operation) {
+			case load:
+				Ldresult = containingProcessor.getMainMemory().getWord(aluresult);
+				break;
+			case store:
+				containingProcessor.getMainMemory().setWord(aluresult, containingProcessor.getRegisterFile().getValue(rs1));
+				break;
+			default:
+				break;
+		
+		}
+		System.out.println("ldresult "+Ldresult);
+		System.out.println("aluresult "+aluresult);
+		MA_RW_Latch.setRs1(rs1);
+		MA_RW_Latch.setRs2(rs2);
+		MA_RW_Latch.setRd(rd);
+		MA_RW_Latch.setAluResult(aluresult);
+		MA_RW_Latch.set_ldresult(Ldresult);
+		MA_RW_Latch.setInstruction(Instruction);
+		MA_RW_Latch.setrem(rem);
+
+		EX_MA_Latch.setMA_enable(false);
+		MA_RW_Latch.setRW_enable(true);
 	}
 
 }
